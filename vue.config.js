b0fc4e396c7569ce3eee90080779edbb81d10e55
Amadeusz <dluzniewskiamadeusz@gmail.com>
2025-02-17 22:55:50 +0100

    Add CNAME file with my domain

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  transpileDependencies: true,
+  lintOnSave: false,
+
+  chainWebpack: (config) => {
+    config.module
+      .rule('ts')
+      .use('babel-loader')
+      .loader('babel-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          plugins: [
+            ["@babel/plugin-proposal-decorators", { "legacy": true }],
+            ["@babel/plugin-proposal-class-properties", { "loose": true }]
+          ],
+        };
+      });
+  },
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+});
